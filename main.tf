d0ef5cfc913a6490ed62e2738fd17c03d706a391
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-11-01 13:02:22 +0100

    WIP

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 terraform {
   required_providers {
-    snowflake = {
+    snowflake.sysadmin= {
       source = "Snowflake-Labs/snowflake"
       version = "~>0.97"
     }
+snowflake.useradmin = {
+      source = "Snowflake-Labs/snowflake"
+      version = "~>0.97"
+    }
+snowflake.securityadmin = {
+      source = "Snowflake-Labs/snowflake"
+      version = "~>0.97"
+    }
+
   }
 }
+
+
