75663292d8c612536758627679bc7fc511cdaeb0
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-04 14:48:29 +0100

    WIP

--- databases.tf
+++ databases.tf
@@ -2,7 +2,7 @@ resource "snowflake_database" "default_database" {
     provider = snowflake.sysadmin
     name = "DDS_{var.project_name}"
     comment = "The database containing project dataproducts"
-    data_retention_time_in_days = var.default_retention_time
+    data_retention_time_in_days = var.default_dds_retention_time
     }
 
 
