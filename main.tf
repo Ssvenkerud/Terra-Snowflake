56127ccfaca0eb9ef0d15872749d4a01bc42e998
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-04 11:36:13 +0100

    WIP

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     snowflake = {
       source = "Snowflake-Labs/snowflake"
-      version = ">= 0.99.0"
       configuration_aliases = [
         snowflake.sysadmin,
         snowflake.useradmin,
