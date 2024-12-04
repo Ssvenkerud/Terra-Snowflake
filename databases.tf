022914998a9e5aaf6a92af10bf357d892f05b69b
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-04 15:19:51 +0100

    WIP

--- databases.tf
+++ databases.tf
@@ -1,8 +1,31 @@
 resource "snowflake_database" "default_database" {
     provider = snowflake.sysadmin
-    name = "DDS_{var.project_name}"
+    name = "DDS_${var.project_name}"
     comment = "The database containing project dataproducts"
     data_retention_time_in_days = var.default_dds_retention_time
     }
 
 
+resource "snowlake_database" "prod_source_database"{
+    provider = snowflake.sysadmin
+    for_each = var.snowflake_prod_source_databases
+    name = "SOURCE_${each.value.source}"
+    comment = "Production source database"
+    data_retention_time_in_days = each.value.data_retention_days
+    }
+
+resource "snowlake_database" "dev_source_database"{
+    provider = snowflake.sysadmin
+    for_each = var.snowflake_dev_source_databases
+    name = "DEV_SOURCE_${each.value.source}"
+    comment = "development source database"
+    data_retention_time_in_days = each.value.data_retention_days
+    }
+
+resource "snowflake_database" "delivery_database" {
+    provider = snowflake.sysadmin
+    for_each = var.snowflake_delivery_databases
+    name = "DDS_${each.value.name}"
+    comment = "delivery database"
+    data_retention_time_in_days = each.value.data_retention_days
+    }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,27 @@ variable "default_dds_retention_time" {
  description = "The retention time for data within the defaul domain data store"
  type = number 
 }
+variable "snowflake_prod_source_databases" {
+  type = list(object({
+    source = string
+    data_retention_days = string
+  }))
+  default = []
+}
+
+variable "snowflake_dev_source_databases" {
+  type = list(object({
+    source = string
+    data_retention_days = string
+  }))
+  default = []
+}
+
+variable "snowflake_delivery_databases" {
+  type = list(object({
+    name = string
+    data_retention_days = string
+  }))
+  default = []
+}
+
