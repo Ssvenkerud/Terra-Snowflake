a23d888bd23c90641319009b02fcce55294b120f
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-04 14:36:35 +0100

    WIP

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "project_name" {
   default = "my-project"
 } 
+
+variable "default_dds_retention_time" {
+ description = "The retention time for data within the defaul domain data store"
+ type = number 
+}
