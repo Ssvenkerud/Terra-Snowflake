14e8636bcabfe6ee4c9f7190912f66aca79e0c9a
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-05 13:45:46 +0100

    wip

--- tag_assosiation.tf
+++ tag_assosiation.tf
@@ -2,7 +2,7 @@ resource "snowflake_tag_association" "db_association" {
   provider = snowflake.sysadmin
 
   object_identifier {
-    name = snowflake_database.prod_source_database[*].fully_qualified_name
+    name = snowflake_database.prod_source_database[*].name
   }
   object_type = "DATABASE"
   tag_id      = snowflake_tag.billing_tag.id
