d5eb86f0b2964ef9f2f6b006b2f79ac98b8ee0dd
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-05 10:50:48 +0100

    WIP

--- users.tf
+++ users.tf
@@ -3,7 +3,6 @@ resource snowflake_service_user sys_loader {
     for_each ={ for user in var.snowflake_data_loader : user.source => user }
     name = "SYS_LOADER_${each.value.source}"
     login_name = "SYS_LOADER_${each.value.source}"
-    must_change_password = false
     comment = "system user for data loading"
     default_warehouse ="LOADING_DATA_${each.value.source}"
     default_role = "LOADER_${each.value.source}"
@@ -18,7 +17,6 @@ resource snowflake_service_user sys_dbt_user {
     count = var.snowflake_dbt_enabled ? 1 : 0
     name = "SYS_DBT_${var.project_name}"
     login_name = "SYS_DBT_${var.project_name}"
-    must_change_password = false
     comment = "system user for data loading"
     #abort_detached_query = true
     #client_session_keep_alive = false
@@ -30,7 +28,6 @@ resource snowflake_service_user sys_powerbi_user {
     count = var.snowflake_powerbi_enabled ? 1 : 0
     name = "SYS_POWERBI_${var.project_name}"
     login_name = "SYS_POWERBI_${var.project_name}"
-    must_change_password = false
     comment = "system user for data loading"
     #abort_detached_query = true
     #client_session_keep_alive = false
@@ -43,7 +40,6 @@ resource snowflake_service_user sys_permifrost_user {
     count = var.snowflake_permifrost_enabled ? 1 : 0
     name = "SYS_PERMIFROST"
     login_name = "SYS_PERMIFROST"
-    must_change_password = false
     comment = "system user for data loading"
     default_role = "SECURITYADMIN"
     #abort_detached_query = true
