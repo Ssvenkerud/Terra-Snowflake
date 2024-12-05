d6e5475aac1186fd88cd29276676d2501c9795f1
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-05 08:07:42 +0100

    adding Roles and warehouses"

---
+++ roles.tf
@@ -0,0 +1,104 @@
+#########################
+## Project based Roles ##
+#########################
+
+resource "snowflake_account_role" "sso_data_engineer" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 1:0
+    name = "DATA_ENGINEER_${var.project_name}"
+}
+
+resource "snowflake_account_role" "sso_data_analyst" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 1:0
+    name = "DATA_READER_${var.project_name}"
+}
+
+resource "snowflake_account_role" "sso_data_domain_admin" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 1:0
+    name = "DATA_${var.project_name}_ADMIN"
+}
+
+resource "snowflake_account_role" "non_sso_data_engineer" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 0:1
+    name = "DATA_ENGINEER_${var.project_name}"
+}
+
+resource "snowflake_account_role" "non_sso_data_analyst" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 0:1
+    name = "DATA_READER_${var.project_name}"
+}
+
+resource "snowflake_account_role" "non_sso_data_domain_admin" {
+    provider = snowflake.useradmin
+    count = var.snowflake_sso_integration ? 0:1
+    name = "DATA_${var.project_name}_ADMIN"
+}
+
+resource "snowflake_account_role" "Powerbi_role" {
+  provider = snowflake.useradmin
+  count = var.snowflake_powerbi_enabled ? 1:0
+  name = "POWERBI_${var.project_name}"
+}
+
+resource "snowflake_account_role" "transformer_role" {
+    provider = snowflake.useradmin
+    name = "TRANSFORMER_${var.project_name}"
+    comment = "role for the sytem user for production transformation"
+}
+
+resource "snowflake_account_role" "extra_roles" {
+  provider = snowflake.useradmin
+  for_each = toset(var.snowflake_additional_roles)
+  name = each.key
+}
+
+#############################
+## AR roles for databases ##
+#############################
+
+resource "snowflake_account_role" "ar_db_read" {
+  provider = snowflake.useradmin
+  for_each = concat(var.snowflake_delivery_databases,var.snowflake_prod_source_databases)
+  name = "AR_DB_${each.key}_R"
+}
+
+resource "snowflake_account_role" "ar_db_write" {
+  provider = snowflake.useradmin
+  for_each = concat(var.snowflake_delivery_databases,var.snowflake_prod_source_databases)
+  name = "AR_DB_${each.key}_W"
+}
+
+resource "snowflake_account_role" "dev_ar_db_read" {
+  provider = snowflake.useradmin
+  for_each = concat(var.snowflake_delivery_databases,var.snowflake_prod_source_databases)
+  name = "DEV_AR_DB_${each.key}_R"
+}
+
+resource "snowflake_account_role" "dev_ar_db_write" {
+  provider = snowflake.useradmin
+  for_each = concat(var.snowflake_delivery_databases,var.snowflake_prod_source_databases)
+  name = "DEV_AR_DB_${each.key}_W"
+}
+
+resource "snowflake_account_role" "ar_schema_read" {
+  provider = snowflake.useradmin
+  for_each = {for role in var.snowflake_schema_role_read : role.name => role}
+  name = "AR_SCHEMA_${each.value.name}_R"
+}
+
+#############################
+## System Functional Roles ##
+#############################
+
+
+resource "snowflake_database_role" "loader_role" {
+    provider = snowflake.useradmin
+    for_each = { for db in var.snowflake_prod_source_databases : db.source => db }
+    name = "LOADER_${each.value.source}"
+    database = snowflake_database.prod_source_database[each.value.source]
+    comment = "role for dataloaders"
+}
--- variables.tf
+++ variables.tf
@@ -2,6 +2,33 @@ variable "project_name" {
   default = "my-project"
 } 
 
+variable "snowflake_dbt_enabled" {
+  type = bool
+
+  default = false
+}
+
+variable "snowflake_prefect_enabled" {
+  type = bool
+
+  default = false
+}
+
+variable "snowflake_powerbi_enabled" {
+  type = bool
+
+  default = false
+}
+variable "snowflake_permifrost_enabled" {
+  type = bool
+
+  default = false
+}
+variable "snowflake_sso_integration" {
+  default = []
+}
+
+
 variable "default_dds_retention_time" {
  description = "The retention time for data within the defaul domain data store"
  type = number 
@@ -10,6 +37,7 @@ variable "snowflake_prod_source_databases" {
   type = list(object({
     source = string
     data_retention_days = string
+    delete_protection = true
   }))
   default = []
 }
@@ -18,6 +46,8 @@ variable "snowflake_dev_source_databases" {
   type = list(object({
     source = string
     data_retention_days = string
+    delete_protection = true
+
   }))
   default = []
 }
@@ -26,7 +56,68 @@ variable "snowflake_delivery_databases" {
   type = list(object({
     name = string
     data_retention_days = string
+    delete_protection = true
+
   }))
   default = []
 }
+variable "snowflake_data_loader" { 
+ type = list(object({
+    source = string
+    size = string
+    auto_suspend = number
+    max_cluster_count = number
+    min_cluster_count = number
+    max_concurrency_level = number
+  }))
+  default = [] 
+}
 
+variable "snowflake_prod_transformer" { 
+ type = list(object({
+    name = string
+    size = string
+    auto_suspend = number
+    max_cluster_count = number
+    min_cluster_count = number
+    max_concurrency_level = number
+  }))
+  default = [] 
+}
+
+variable "snowflake_dev_transformer" { 
+ type = list(object({
+    name = string
+    size = string
+    auto_suspend = number
+    max_cluster_count = number
+    min_cluster_count = number
+    max_concurrency_level = number
+  }))
+  default = [] 
+}
+
+
+variable "snowflake_extra_warehouses" { 
+ type = list(object({
+    name = string
+    size = string
+    auto_suspend = number
+    max_cluster_count = number
+    min_cluster_count = number
+    max_concurrency_level = number
+  }))
+  default = [] 
+}
+
+variable "snowflake_additional_roles" {
+    type = list()
+    default = []
+}
+
+variable "snowflake_schema_role_read" {
+    type = list(object({
+        name = string
+    }))
+    default = []
+}
