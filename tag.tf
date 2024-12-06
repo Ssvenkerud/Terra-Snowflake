2f4266839606079f2c375dea2b8080d2bf32ca73
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-12-06 18:32:02 +0100

    wip

--- tag.tf
+++ tag.tf
@@ -1,7 +1,7 @@
 resource "snowflake_tag" "billing_tag" {
  provider = snowflake.sysadmin
  database = "DDS_${var.project_name}"
- schema = "INFORMATION_SCHEMA"
+ schema = "PUBLIC"
  name = "PROJECT"
  comment = "Tag used to ascribe ownership of objects and resoures to projects"
 }
