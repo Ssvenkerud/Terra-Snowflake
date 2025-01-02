0687fb38ff25599a6b63825cda07a8cc009cb05b
Simen Svenkerud <simen@ullrscrafts.com>
2025-01-02 17:28:09 +0100

    clone attemt 2

--- clone_tasks.tf
+++ clone_tasks.tf
@@ -1,20 +1,15 @@
-resource "snowflake_task" "Clone_prod_source" {
-  provider = snowflake.sysadmin
-  for_each = { for db in var.snowflake_prod_source_databases : db.name => db }
-
-
-  comment = "Clone dev data from prod databases"
-
-  started = true
-
-  database  = snowflake_database.prod_source_database[each.key].name
-  schema    = "PUBLIC"
-  warehouse = snowflake_warehouse.sys_warehouse.id
-
-  name = "Clone ${each.value.name} to Dev enviroment"
+resource "snowflake_task" "Source_clone" {
+  provider  = snowflake.sysadmin
+  for_each  = { for db in var.snowflake_prod_source_databases : db.name => db }
+  database  = "SYSTEM"
+  schema    = "dev_clones"
+  name      = "Clone ${each.value.name} to prod"
+  warehouse = snowflake_warehouse.sys_warehouse.fully_qualified_name
+  started   = true
   schedule {
     minutes = 5
   }
   sql_statement = "CREATE OR REPLACE DATABASE DEV_SOURCE_${each.value.name} CLONE ${snowflake_database.prod_source_database[each.key].name};"
   depends_on    = [snowflake_warehouse.sys_warehouse]
+
 }
