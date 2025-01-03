9879a42c0602606106f1efcd12d5e455395db90d
Simen Svenkerud <simen@ullrscrafts.com>
2025-01-03 15:55:50 +0100

    WIP

--- users.tf
+++ users.tf
@@ -57,10 +57,10 @@ resource "snowflake_grant_account_role" "Permifrost_grant" {
   user_name = snowflake_service_user.sys_permifrost_user[0].name
 }
 
-resource "snowflake_grant_privileges_to_role" "Permifrost_Warehouse" {
-  provider   = snowflake.securityadmin
-  privileges = ["USAGE"]
-  role_name  = "SECURITYADMIN"
+resource "snowflake_grant_privileges_to_account_role" "Permifrost_Warehouse" {
+  provider          = snowflake.securityadmin
+  privileges        = ["USAGE"]
+  account_role_name = "SECURITYADMIN"
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = "SYSTEM"
