8e772b1a7e20634c6370f96595fe4c4af6e140e7
Simen Svenkerud <simen@ullrscrafts.com>
2025-01-03 09:18:12 +0100

    WIP

--- tag_assosiation.tf
+++ tag_assosiation.tf
@@ -1,7 +1,7 @@
 resource "snowflake_tag_association" "dds_db_association" {
   provider = snowflake.sysadmin
 
-  object_identifiers = [snowflake_database.default_database[var.project_name].fully_qualified_name]
+  object_identifiers = [snowflake_database.default_database[var.project_name]]
   object_type        = "DATABASE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -12,7 +12,7 @@ resource "snowflake_tag_association" "prod_source_db_association" {
 
   for_each = { for db in var.snowflake_prod_source_databases : db.name => db }
 
-  object_identifiers = [snowflake_database.prod_source_database[each.value.name].fully_qualified_name]
+  object_identifiers = [snowflake_database.prod_source_database[each.value.name]]
   object_type        = "DATABASE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -23,7 +23,7 @@ resource "snowflake_tag_association" "dev_source_db_association" {
 
   for_each = { for db in var.snowflake_dev_source_databases : db.name => db }
 
-  object_identifiers = [snowflake_database.dev_source_database[each.value.name].fully_qualified_name]
+  object_identifiers = [snowflake_database.dev_source_database[each.value.name]]
   object_type        = "DATABASE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -34,7 +34,7 @@ resource "snowflake_tag_association" "delivery_db_association" {
 
   for_each = { for db in var.snowflake_delivery_databases : db.name => db }
 
-  object_identifiers = [snowflake_database.delivery_database[each.value.name].fully_qulaified_name]
+  object_identifiers = [snowflake_database.delivery_database[each.value.name]]
   object_type        = "DATABASE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -46,7 +46,7 @@ resource "snowflake_tag_association" "loading_warehouse_association" {
 
   for_each = { for wh in var.snowflake_data_loader : wh.source => wh }
 
-  object_identifiers = [snowflake_warehouse.default_loading_warehouse[each.value.source].fully_qualified_name]
+  object_identifiers = [snowflake_warehouse.default_loading_warehouse[each.value.source]]
   object_type        = "WAREHOUSE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -58,7 +58,7 @@ resource "snowflake_tag_association" "prod_transformer_association" {
 
   for_each = { for wh in var.snowflake_prod_transformer : wh.name => wh }
 
-  object_identifiers = [snowflake_warehouse.default_prod_transformer[each.value.name].fully_qualiaified_name]
+  object_identifiers = [snowflake_warehouse.default_prod_transformer[each.value.name]]
   object_type        = "WAREHOUSE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
   tag_value          = var.project_name
@@ -82,7 +82,7 @@ resource "snowflake_tag_association" "extra_warehouses_association" {
 
   for_each = { for wh in var.snowflake_extra_warehouses : wh.name => wh }
 
-  object_identifiers = [snowflake_warehouse.exstra_warehouse[each.value.name].fully_qualified_name]
+  object_identifiers = [snowflake_warehouse.exstra_warehouse[each.value.name]]
   object_type        = "WAREHOUSE"
   tag_id             = "SYSTEM.PUBLIC.PROJECT"
 
