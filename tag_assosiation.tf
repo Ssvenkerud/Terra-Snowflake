62b344ebf9d551e0b4b35dae5068cec7062abd14
Simen Svenkerud <simen@ullrscrafts.com>
2025-01-03 08:38:05 +0100

    changing billing qualifier

--- tag_assosiation.tf
+++ tag_assosiation.tf
@@ -3,7 +3,7 @@ resource "snowflake_tag_association" "dds_db_association" {
 
   object_identifiers = [snowflake_database.default_database.fully_qualified_name]
   object_type        = "DATABASE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
 }
 
@@ -14,7 +14,7 @@ resource "snowflake_tag_association" "prod_source_db_association" {
 
   object_identifiers = [snowflake_database.prod_source_database[each.value.name].fully_qualified_name]
   object_type        = "DATABASE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
 }
 
@@ -25,7 +25,7 @@ resource "snowflake_tag_association" "dev_source_db_association" {
 
   object_identifiers = [snowflake_database.dev_source_database[each.value.name].fully_qualified_name]
   object_type        = "DATABASE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
 }
 
@@ -36,7 +36,7 @@ resource "snowflake_tag_association" "delivery_db_association" {
 
   object_identifiers = [snowflake_database.delivery_database[each.value.name].fully_qulaified_name]
   object_type        = "DATABASE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
   depends_on         = [snowflake_tag.billing_tag]
 }
@@ -49,7 +49,7 @@ resource "snowflake_tag_association" "loading_warehouse_association" {
 
   object_identifiers = [snowflake_warehouse.default_loading_warehouse[each.value.source].fully_qualified_name]
   object_type        = "WAREHOUSE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
   depends_on         = [snowflake_tag.billing_tag]
 }
@@ -62,7 +62,7 @@ resource "snowflake_tag_association" "prod_transformer_association" {
 
   object_identifiers = [snowflake_warehouse.default_prod_transformer[each.value.name].fully_qualiaified_name]
   object_type        = "WAREHOUSE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
   depends_on         = [snowflake_tag.billing_tag]
 }
@@ -75,7 +75,7 @@ resource "snowflake_tag_association" "dev_transformer_association" {
 
   object_identifiers = [snowflake_warehouse.default_dev_transformer[each.value.name]]
   object_type        = "WAREHOUSE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
   depends_on         = [snowflake_tag.billing_tag]
 }
@@ -88,7 +88,7 @@ resource "snowflake_tag_association" "extra_warehouses_association" {
 
   object_identifiers = [snowflake_warehouse.exstra_warehouse[each.value.name].fully_qualified_name]
   object_type        = "WAREHOUSE"
-  tag_id             = snowflake_tag.billing_tag[1].id
+  tag_id             = snowflake_tag.billing_tag[0].id
   tag_value          = var.project_name
   depends_on         = [snowflake_tag.billing_tag]
 }
