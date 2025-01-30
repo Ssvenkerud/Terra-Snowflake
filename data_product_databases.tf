d83e9d6359d1373735f298d5c4c4b225194accaf
Simen Svenkerud <simen@ullrscrafts.com>
2025-01-30 10:46:09 +0100

    WIP

--- data_product_databases.tf
+++ data_product_databases.tf
@@ -14,8 +14,10 @@ resource "snowflake_database_role" "central_data_products_role_w" {
 
   database = "DATA_PRODUCTS"
 
-  name    = "AR_DB_DATA_PRODUCTS_W"
-  comment = "database write access role"
+  name       = "AR_DB_DATA_PRODUCTS_W"
+  comment    = "database write access role"
+  depends_on = [snowflake_database.central_data_product_database]
+
 }
 
 resource "snowflake_database_role" "central_data_products_role_r" {
@@ -24,8 +26,9 @@ resource "snowflake_database_role" "central_data_products_role_r" {
 
   database = "DATA_PRODUCTS"
 
-  name    = "AR_DB_DATA_PRODUCTS_R"
-  comment = "database read access role"
+  name       = "AR_DB_DATA_PRODUCTS_R"
+  comment    = "database read access role"
+  depends_on = [snowflake_database.central_data_product_database]
 }
 
 resource "snowflake_database" "project_data_products_database" {
@@ -36,15 +39,24 @@ resource "snowflake_database" "project_data_products_database" {
   comment                     = "This database acts as as central sharing layer for data between the teams"
   data_retention_time_in_days = var.default_dds_retention_time
 }
+resource "snowflake_database" "dev_project_data_products_database" {
+  provider = snowflake.sysadmin
+  count    = var.snowflake_project_data_products_setup ? 1 : 0
 
+  name                        = "DEV_DATA_PRODUCTS_${var.project_name}"
+  comment                     = "This database acts as as central sharing layer for data between the teams"
+  data_retention_time_in_days = var.default_dds_retention_time
+}
 resource "snowflake_database_role" "project_data_products_role_w" {
   provider = snowflake.sysadmin
   count    = var.snowflake_project_data_products_setup ? 1 : 0
 
   database = "DATA_PRODUCTS_${var.project_name}"
 
-  name    = "AR_DB_DATA_PRODUCTS_${var.project_name}_W"
-  comment = "database write access role"
+  name       = "AR_DB_DATA_PRODUCTS_${var.project_name}_W"
+  comment    = "database write access role"
+  depends_on = [snowflake_database.project_data_products_database]
+
 }
 
 resource "snowflake_database_role" "project_data_products_role_r" {
@@ -53,6 +65,29 @@ resource "snowflake_database_role" "project_data_products_role_r" {
 
   database = "DATA_PRODUCTS_${var.project_name}"
 
-  name    = "AR_DB_DATA_PRODUCTS_${var.project_name}_R"
-  comment = "database read access role"
+  name       = "AR_DB_DATA_PRODUCTS_${var.project_name}_R"
+  comment    = "database read access role"
+  depends_on = [snowflake_database.project_data_products_database]
+}
+
+resource "snowflake_database_role" "dev_project_data_products_role_w" {
+  provider = snowflake.sysadmin
+  count    = var.snowflake_project_data_products_setup ? 1 : 0
+
+  database = "DEV_DATA_PRODUCTS_${var.project_name}"
+
+  name       = "DEV_AR_DB_DATA_PRODUCTS_${var.project_name}_W"
+  comment    = "database write access role"
+  depends_on = [snowflake_database.dev_project_data_products_database]
+}
+
+resource "snowflake_database_role" "dev_project_data_products_role_r" {
+  provider = snowflake.sysadmin
+  count    = var.snowflake_project_data_products_setup ? 1 : 0
+
+  database = "DEV_DATA_PRODUCTS_${var.project_name}"
+
+  name       = "DEV_AR_DB_DATA_PRODUCTS_${var.project_name}_R"
+  comment    = "database read access role"
+  depends_on = [snowflake_database.dev_project_data_products_database]
 }
