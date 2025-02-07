c1f0a22177e017ded794269ba586dc81a437e301
Simen Svenkerud <simen@ullrscrafts.com>
2025-02-07 09:28:52 +0100

    aed povider to saml integration

--- intgrations.tf
+++ intgrations.tf
@@ -8,12 +8,13 @@ resource "snowflake_oauth_integration_for_custom_clients" "dbt_cloud" {
   enabled                      = "true"
   blocked_roles_list           = ["ACCOUNTADMIN", "SECURITYADMIN", "SYSADMIN", "USERADMIN", "DATA_ADMIN"]
   oauth_issue_refresh_tokens   = "true"
-  oauth_refresh_token_validity = 7776000
+  oauth_refresh_token_validity = var.dbt_oauth_issue_refresh_tokens
   comment                      = "integration for DBT cloud user authenticaition"
 }
 
 
 resource "snowflake_saml2_integration" "OKTA_SSO" {
+  provider                  = snowflake.accountadmin
   count                     = var.snowflake_okta_sso_setup ? 1 : 0
   allowed_email_patterns    = var.allowed_sso_email_patterns
   allowed_user_domains      = var.allowed_sso_email_domain
@@ -24,7 +25,6 @@ resource "snowflake_saml2_integration" "OKTA_SSO" {
   saml2_force_authn         = false
   saml2_issuer              = var.sso_idp_entity_id
   saml2_provider            = "OKTA"
-  saml2_snowflake_x509_cert = file("snowflake_cert.pem")
   saml2_sso_url             = var.sso_url
-  saml2_x509_cert           = file("cert.pem")
+  saml2_x509_cert           = file(var.saml2_x509_cert)
 }
