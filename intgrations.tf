b1298839dfd1913e6ab75adac8ffbf1883a2bdd2
Simen Svenkerud <simen@ullrscrafts.com>
2025-02-07 09:48:34 +0100

    WIP

--- intgrations.tf
+++ intgrations.tf
@@ -16,8 +16,6 @@ resource "snowflake_oauth_integration_for_custom_clients" "dbt_cloud" {
 resource "snowflake_saml2_integration" "OKTA_SSO" {
   provider                  = snowflake.accountadmin
   count                     = var.snowflake_okta_sso_setup ? 1 : 0
-  allowed_email_patterns    = var.allowed_sso_email_patterns
-  allowed_user_domains      = var.allowed_sso_email_domain
   comment                   = "Terraform managed integration"
   enabled                   = true
   name                      = "SSO_OKTA_INTEGRATION"
