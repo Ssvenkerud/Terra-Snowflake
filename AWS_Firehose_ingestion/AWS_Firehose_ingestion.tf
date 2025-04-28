19d4527dc2f9062e1d3fb608ab45d58764f3f7f8
Simen Svenkerud <simen@ullrscrafts.com>
2025-04-28 13:16:22 +0200

    WIP

--- AWS_Firehose_ingestion/AWS_Firehose_ingestion.tf
+++ AWS_Firehose_ingestion/AWS_Firehose_ingestion.tf
@@ -2,52 +2,20 @@
 ## Firehose source data Ingedtion ##
 ####################################
 
-
-
-variable "snowflake_firehose_ingestion_tables" {
-  type = map(map(list(string)))
-  default = {
-    test_db = {
-      "Tables" = ["test_tbl", "tbl_two"]
-    }
-    test_db2 = {
-      "Tables" = ["test_tbl", "tbl_two"]
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = ">= 0.99.0"
+      configuration_aliases = [
+        snowflake.sysadmin,
+        snowflake.useradmin,
+        snowflake.securityadmin
+      ]
     }
   }
 }
 
-variable "snowflake_firehose_ingestion_databases" {
-  type = list(object({}))
-  default = {
-    database       = string
-    retention_days = string
-  }
-}
-
-variable "snowflake_firehose_user" {
-  description = "Settings for warehouse that are used for the data ingestion process. "
-  type = object({
-    name      = string
-    warehouse = string
-  })
-}
-
-variable "aws_firehose_public_key1" {
-  description = "Primary public key for authentication to Snowflake, Must be on 1 line without header and trailer"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_firehose_public_key2" {
-  description = "Secondary public key for authentication to Snowflake, Must be on 1 line without header and trailer"
-  type        = string
-  sensitive   = true
-}
-
-variable "project_name" {
-  description = "The name of the project that is the holder of the terraform state."
-  default     = "my-project"
-}
 
 locals {
   firehose_ingestion_tables = flatten([
