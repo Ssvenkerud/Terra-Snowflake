c5c3044b50544191825324f2ab32894157f9a92e
Simen Svenkerud <simen@ullrscrafts.com>
2025-05-28 10:28:57 +0200

    version pin

--- AWS_s3_external_stage_ingestion/s3_external_stage_ingestion.tf
+++ AWS_s3_external_stage_ingestion/s3_external_stage_ingestion.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = ">= 2.1.0"
+      version = ">= 0.99.0"
       configuration_aliases = [
         snowflake.sysadmin,
         snowflake.useradmin,
