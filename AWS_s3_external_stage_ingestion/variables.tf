4aa07e88fbcf62b30888f3b2e1d208ab8c743a8a
Simen Svenkerud <simen@ullrscrafts.com>
2025-05-28 10:53:48 +0200

    add retention time

--- AWS_s3_external_stage_ingestion/variables.tf
+++ AWS_s3_external_stage_ingestion/variables.tf
@@ -3,7 +3,8 @@
 variable "snowflake_s3_sources" {
   type = list(map(string))
   default = [{
-    "source" : "eir",
-    "s3_location" : "s3://com.example.bucket/prefix"
+    source : "eir",
+    s3_location : "s3://com.example.bucket/prefix"
+    retention_days = "value2"
   }]
 }
