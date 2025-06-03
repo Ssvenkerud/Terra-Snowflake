d4d0aebdbc1652f18bdf69eefe357062c653757c
Simen Svenkerud <simen@ullrscrafts.com>
2025-06-03 19:37:36 +0200

    adding central data product dev database

--- base/data_product_databases.tf
+++ base/data_product_databases.tf
@@ -7,7 +7,14 @@ resource "snowflake_database" "central_data_product_database" {
   comment                     = "This database acts as as central sharing layer for data between the teams"
   data_retention_time_in_days = var.default_dds_retention_time
 }
+resource "snowflake_database" "central_dev_data_product_database" {
+  provider = snowflake.sysadmin
+  count    = var.snowflake_central_data_products_setup ? 1 : 0
 
+  name                        = "DEV_DATA_PRODUCTS"
+  comment                     = "This database acts as as central sharing layer for data between the teams"
+  data_retention_time_in_days = var.default_dds_retention_time
+}
 resource "snowflake_account_role" "central_data_products_role_w" {
   provider = snowflake.securityadmin
   count    = var.snowflake_central_data_products_setup ? 1 : 0
@@ -28,6 +35,27 @@ resource "snowflake_account_role" "central_data_products_role_r" {
   comment    = "database read access role"
   depends_on = [snowflake_database.central_data_product_database]
 }
+resource "snowflake_account_role" "central_dev_data_products_role_w" {
+  provider = snowflake.securityadmin
+  count    = var.snowflake_central_data_products_setup ? 1 : 0
+
+
+  name       = "DEV_AR_DB_DATA_PRODUCTS_W"
+  comment    = "database write access role"
+  depends_on = [snowflake_database.central_dev_data_product_database]
+
+}
+
+resource "snowflake_account_role" "central_dev_data_products_role_r" {
+  provider = snowflake.securityadmin
+  count    = var.snowflake_central_data_products_setup ? 1 : 0
+
+
+  name       = "DEV_AR_DB_DATA_PRODUCTS_R"
+  comment    = "database read access role"
+  depends_on = [snowflake_database.central_dev_data_product_database]
+}
+
 
 resource "snowflake_database" "project_data_products_database" {
   provider = snowflake.sysadmin
