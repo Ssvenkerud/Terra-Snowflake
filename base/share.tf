9a780f3a99abcd24d206d289616b63616936ef95
Simen Svenkerud <simen@ullrscrafts.com>
2025-06-26 14:27:29 +0200

    deiabled terraform share

--- base/share.tf
+++ base/share.tf
@@ -1,10 +1,4 @@
-resource "snowflake_share" "outgoing_share" {
-  provider   = snowflake.accountadmin # notice the provider fields
-  for_each   = { for share in var.snowfake_outgoing_share : share.name => share }
-  name       = each.value.name
-  accounts   = each.value.outgoing_accounts
-  depends_on = [snowflake_database.outgoing_share_database]
-}
+
 
 resource "snowflake_database" "outgoing_share_database" {
   provider = snowflake.accountadmin
@@ -12,11 +6,3 @@ resource "snowflake_database" "outgoing_share_database" {
   name     = "SHARE_OUTGOING_${each.value.name}"
 }
 
-resource "snowflake_grant_privileges_to_share" "outgoing_share_grant" {
-  provider    = snowflake.securityadmin
-  for_each    = { for share in var.snowfake_outgoing_share : share.name => share }
-  to_share    = snowflake_share.outgoing_share[each.value.name].name
-  privileges  = ["USAGE"]
-  on_database = snowflake_database.outgoing_share_database[each.value.name].name
-  depends_on  = [snowflake_database.outgoing_share_database, snowflake_share.outgoing_share]
-}
