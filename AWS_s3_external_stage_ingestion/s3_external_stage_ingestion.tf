eda4839e480750c5eb80ded64e061a42a497a81a
Simen Svenkerud <simen@ullrscrafts.com>
2025-10-08 08:47:57 +0200

    Removing source cdb creation from external stage.

--- AWS_s3_external_stage_ingestion/s3_external_stage_ingestion.tf
+++ AWS_s3_external_stage_ingestion/s3_external_stage_ingestion.tf
@@ -14,31 +14,6 @@ terraform {
 }
 
 
-resource "snowflake_database" "s3_source_database" {
-  provider                    = snowflake.sysadmin
-  for_each                    = { for db in var.snowflake_s3_sources : db.source => db }
-  name                        = "SOURCE_${each.value.source}"
-  comment                     = "Production source database"
-  data_retention_time_in_days = each.value.data_retention_days
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-
-
-resource "snowflake_schema" "s3_landing_landing_schema" {
-  provider     = snowflake.sysadmin
-  for_each     = { for db in var.snowflake_s3_sources : db.source => db }
-  name         = "LANDING"
-  database     = "SOURCE_${each.value.source}"
-  comment      = "Schema containin the landing zone for data ingested via external stage for the source: ${each.value.source}"
-  is_transient = false
-  depends_on = [
-    snowflake_database.s3_source_database,
-  ]
-}
-
 
 resource "snowflake_stage" "S3_ingestion_stage" {
   provider            = snowflake.accountadmin
@@ -49,37 +24,6 @@ resource "snowflake_stage" "S3_ingestion_stage" {
   schema              = "LANDING"
   storage_integration = "S3_STORAGE"
   directory           = "ENABLE = true"
-
-}
-
-
-resource "snowflake_account_role" "ar_db_source_read" {
-  provider = snowflake.securityadmin
-  for_each = { for db in var.snowflake_s3_sources : db.source => db }
-
-  name = "AR_DB_SOURCE_${each.key}_R"
-}
-
-resource "snowflake_account_role" "ar_db_source_write" {
-  provider = snowflake.securityadmin
-  for_each = { for db in var.snowflake_s3_sources : db.source => db }
-
-  name = "AR_DB_SOURCE_${each.key}_W"
-
-}
-
-resource "snowflake_account_role" "dev_ar_db_source_read" {
-  provider = snowflake.securityadmin
-  for_each = { for db in var.snowflake_s3_sources : db.source => db }
-
-  name = "DEV_AR_DB_SOURCE_${each.key}_R"
-}
-
-resource "snowflake_account_role" "dev_ar_db_source_write" {
-  provider = snowflake.securityadmin
-  for_each = { for db in var.snowflake_s3_sources : db.source => db }
-
-  name = "DEV_AR_DB_SOURCE_${each.key}_W"
 
 }
 
